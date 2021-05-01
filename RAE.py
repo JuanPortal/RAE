71345cff2c62526175e2a3f9ee33abac5ed14a58
Juan Portal <78442505+JuanPortal@users.noreply.github.com>
2021-05-01 13:24:00 -0500

    Update RAE.py

--- RAE.py
+++ RAE.py
@@ -13,30 +13,40 @@ async def on_ready():
 
 
 @client.command(pass_context=True)
-async def busca(ctx, arg):
+async def busca(ctx, arg, arg2 = ""):
     try:
-        palabra = arg.lower()
+      palabra = str(arg).lower()
 
-        source = requests.get('https://dle.rae.es/' + palabra + '/').text
+      if str(arg2) != "":
+        palabra = str(arg) + "%20" + str(arg2)      
 
-        soup = BeautifulSoup(source, "lxml")
+      print(palabra)
 
-        article = soup.find("article")
+      source = requests.get('https://dle.rae.es/' + palabra + '/').text
 
-        word_to_show = article.find("header", class_="f").text
+      soup = BeautifulSoup(source, "lxml")
 
-        textote = ""
+      article = soup.find("article")
 
-        for x in article.find_all(
-                "p", {"class": ["j", "j1", "j2", "j3", "j4", "j5", "j6", "l2"]}):
+      word_to_show = article.find("header", class_="f").text
 
-            if len(textote + x.text) < 2048:
-                textote += x.text + "\n\n"
+      textote = ""
 
-        em = discord.Embed(title=word_to_show, description=textote, color=0xFFFF00)
-        await ctx.send(embed=em)
+      for x in article.find_all(
+              "p", {"class": ["j", "j1", "j2", "j3", "j4", "j5", "j6", "l2"]}):
 
+          if len(textote + x.text) < 2048:
+            textote += x.text + "\n\n"
+
+       
+      em = discord.Embed(title=word_to_show, description=textote, color=0xFFFF00)
+      await ctx.send(embed=em)
+    
     except (ValueError, Exception):
+      if str(arg2) == "":
+        await ctx.channel.send(f"{palabra} no se encuentra en el diccionario")
+      else:
+        palabra = palabra.replace("%20", " ")
         await ctx.channel.send(f"{palabra} no se encuentra en el diccionario")
 
 
