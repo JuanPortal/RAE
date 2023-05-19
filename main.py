0b3e24651b95289271d401ae969881517a319a62
Portal <juanportalg@gmail.com>
2023-05-19 14:52:05 -0700

    code updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import urllib.parse
 import lxml
-from dotenv import load_dotenv
+from keep_alive import keep_alive
 
 client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 client.remove_command("help")
@@ -16,33 +16,30 @@ async def on_ready():
     print("RAE ready!")
 
 
-@client.command(pass_context=True, aliases=["buscar", "search", "b", "lookfor"])
-async def busca(ctx, arg, arg2=""):
+@client.command(pass_context=True, aliases=["busca", "b"])
+async def buscar(ctx, arg, arg2=""):
     try:
         palabra = urllib.parse.quote(arg.lower())
         if str(arg2) != "":
             palabra = urllib.parse.quote(str(arg) + " " + str(arg2))
-
-        print(palabra)
 
         url = f"https://dle.rae.es/{palabra}/"
 
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
         soup = BeautifulSoup(webpage, "lxml")
-
         article = soup.find("article")
 
-        word_to_show = article.find("header", class_="f").text
+        palabra_a_mostrar = article.find("header", class_="f").text
 
-        textote = ""
+        significados = ""
 
-        for x in article.find_all("p", {"class": ["j", "j1", "j2", "j3", "j4", "j5", "j6", "l2"]}):
-            if len(textote + x.text) < 2048:
-                textote += x.text + "\n\n"
+        for significado in article.find_all("p", {"class": ["j", "j1", "j2", "j3", "j4", "j5", "j6", "l2"]}):
+            if len(significados + significado.text) < 2048:
+                significados += significado.text + "\n\n"
 
-        em = discord.Embed(title=word_to_show, description=textote, color=0xFFFF00)
-        await ctx.send(embed=em)
+        mostrar = discord.Embed(title=palabra_a_mostrar, description=significados, color=0xFFFF00)
+        await ctx.send(embed=mostrar)
 
     except UnicodeEncodeError:
         await ctx.channel.send(f"Por el momento estamos teniendo problemas con las palabras con tilde; pronto lanzaremos la actualización. ¡Gracias por la paciencia!")
@@ -56,25 +53,23 @@ async def busca(ctx, arg, arg2=""):
             await ctx.channel.send(f"{palabra} no se encuentra en el diccionario")
 
 
-@client.command(pass_context=True, aliases=["pdd", "deldia"])
-async def wotd(ctx):
-    req2 = Request("https://dle.rae.es/", headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req2).read()
-    soup2 = BeautifulSoup(webpage, "lxml")
+@client.command(pass_context=True, aliases=["wotd", "deldia"])
+async def pdd(ctx):
+    req = Request("https://dle.rae.es/", headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, "lxml")
+    article = soup.find("div")
+    palabra = article.find("div", class_="row").find("div", class_="col-sm-4 bloqueIn").find("div", class_="").p.a.text
 
-    article = soup2.find("div")
-
-    wotd = article.find("div", class_="row").find("div", class_="col-sm-4 bloqueIn").find("div", class_="").p.a.text
-
-    embedded = discord.Embed(title="Palabra del día", description=wotd, color=0xFF5733)
-    await ctx.send(embed=embedded)
+    mostrar = discord.Embed(title="Palabra del día", description=palabra, color=0xFF5733)
+    await ctx.send(embed=mostrar)
 
 
 @client.command(pass_context=True)
 async def help(ctx):
-    embedded = discord.Embed(title="Help", description="***$busca*** devuelve el significado de la palabra\n\n***$wotd*** retorna la palabra del día")
-    await ctx.send(embed=embedded)
+    mostrar = discord.Embed(title="Help", description="***$busca*** devuelve el significado de la palabra\n\n***$wotd*** retorna la palabra del día")
+    await ctx.send(embed=mostrar)
 
 
-load_dotenv()
-client.run(os.getenv("TOKEN"))
+keep_alive()
+client.run(os.environ("TOKEN"))
